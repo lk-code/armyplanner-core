b8091ce03c5dd5d85a53594492f188d4c8bedbe0
Lars <lars.kraemer@live.de>
2022-07-10 01:41:48 +0200

    added storageservice for net5 (higher and maui)

--- ArmyPlanner/Extensions/ServiceCollectionExtension.cs
+++ ArmyPlanner/Extensions/ServiceCollectionExtension.cs
@@ -3,6 +3,7 @@ using ArmyPlanner.Services.Codex;
 using ArmyPlanner.Services.Http;
 using ArmyPlanner.Services.Repository;
 using ArmyPlanner.Services.Roster;
+using ArmyPlanner.Services.Storage;
 using ArmyPlanner.Services.Translation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,6 +18,10 @@ namespace ArmyPlanner.Extensions
             services.AddSingleton<ITranslationService, TranslationService>();
             services.AddSingleton<ICodexService, CodexService>();
             services.AddSingleton<IRosterService, RosterService>();
+
+#if NET5_0_OR_GREATER
+            services.AddSingleton<IStorageService, NetStorageService>();
+#endif
 
             return services;
         }
---
+++ ArmyPlanner/Services/Storage/NetStorageService.cs
@@ -0,0 +1,60 @@
+﻿using ArmyPlanner.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ArmyPlanner.Services.Storage
+{
+    public class NetStorageService : IStorageService
+    {
+        #region properties
+
+
+        #endregion
+
+        #region constrcutors
+
+        public NetStorageService()
+        {
+        }
+
+        #endregion
+
+        #region logic
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="folderPath"></param>
+        /// <returns></returns>
+        public async Task<string> GetDataAsync(string filename, string folderPath)
+        {
+            string filePath = Path.Combine(folderPath, filename);
+
+            if (!File.Exists(filePath))
+            {
+                return string.Empty;
+            }
+
+            return await File.ReadAllTextAsync(filePath);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="content"></param>
+        /// <param name="folderPath"></param>
+        /// <returns></returns>
+        public async Task WriteDataAsync(string filename, string content, string folderPath)
+        {
+            Directory.CreateDirectory(folderPath);
+
+            string filePath = Path.Combine(folderPath, filename);
+
+            await File.WriteAllTextAsync(filePath, content);
+        }
+
+        #endregion
+    }
+}
